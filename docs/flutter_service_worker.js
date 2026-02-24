10e46b6a5cc61a9e67f06dd7a86958ed51453eef
Tr3bb3 <g.paauw@trebbe.nl>
2026-02-24 15:54:20 +0100

    Deploy web build: update team names + PDF running score

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.avif": "c3b471619ca4c61b13e21f176706cbff",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "7a485378a6d5f7b242f594e7c6c8291d",
+"flutter_bootstrap.js": "a7c7ee293c5c015b8bf690bb3491835e",
 "icons/Icon-192.avif": "694aaf319d9bbb7e169f12c65a6d8042",
 "icons/Icon-512.avif": "8e09fefd47124d7f26a133158689c616",
 "icons/Icon-maskable-192.avif": "694aaf319d9bbb7e169f12c65a6d8042",
