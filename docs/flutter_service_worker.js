c7779dda2f9d10c71d0a3bb0e42ec8206e47b527
Tr3bb3 <g.paauw@trebbe.nl>
2026-02-24 15:53:37 +0100

    Fix manifest: start_url/scope voor Project Page + icons

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.avif": "c3b471619ca4c61b13e21f176706cbff",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "a8de6be39e253f6354cb07b39d792108",
+"flutter_bootstrap.js": "7a485378a6d5f7b242f594e7c6c8291d",
 "icons/Icon-192.avif": "694aaf319d9bbb7e169f12c65a6d8042",
 "icons/Icon-512.avif": "8e09fefd47124d7f26a133158689c616",
 "icons/Icon-maskable-192.avif": "694aaf319d9bbb7e169f12c65a6d8042",
